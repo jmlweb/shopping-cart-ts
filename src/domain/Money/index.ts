878fd63b50906241684b0bbeda2d4c74d7a76f22
José Manuel Lucas <josemanuel@jmlweb.es>
2021-02-04 12:59:14 +0100

    feat: cart item repository logic

--- src/domain/Money/index.ts
+++ src/domain/Money/index.ts
@@ -1,5 +1,11 @@
 import formatMoney from './formatMoney';
 
+/**
+ * This class handles rounding precision for money amounts
+ * It handles integers that can get formatted later with a defined precision.
+ * This is useful in the case tomorrow the company decides to use an ERP with a different precision
+ * and migrates the DB we just need to change one constant.
+ */
 class Money {
   static of(value: number): Money {
     return new Money(value);
