fbb4b835407ff35a833e1484ba713d9290a5a05e
José Manuel Lucas <josemanuel@jmlweb.es>
2021-02-09 03:18:01 +0100

    feat: Basic implementation on product modal logic

--- src/scenes/ProductDetail/index.tsx
+++ src/scenes/ProductDetail/index.tsx
@@ -1,5 +1,9 @@
 import { FC } from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from 'react-query';
 
+import useAPI from '../../logic/application/API/useAPI';
+import Product from '../../logic/domain/Product';
 import Ground from '../../components/Ground';
 import Container from '../../components/Container';
 import DividedCard from '../../components/DividedCard';
@@ -8,7 +12,11 @@ import ProductImage from './components/ProductImage';
 import ProductDetailData from './components/ProductDetailData';
 import ProductDetailClose from './components/ProductDetailClose';
 
-const ProductDetail: FC = () => (
+type Props = {
+  product: Product
+};
+
+const ProductDetailView: FC<Props> = ({ product }) => (
   <Ground>
     <ProductDetailOverlay />
     <Container>
@@ -16,13 +24,30 @@ const ProductDetail: FC = () => (
         mainContent={<ProductImage />}
         auxContent={(
           <>
-            <ProductDetailData />
+            <ProductDetailData product={product} />
             <ProductDetailClose />
           </>
-)}
+  )}
       />
     </Container>
   </Ground>
 );
+
+type Params = {
+  code: string
+};
+
+const ProductDetail: FC = () => {
+  const { code } = useParams<Params>();
+  const api = useAPI();
+  const { data, error, isLoading } = useQuery(['productDetail', code], () => api.product.get(code));
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error || !data) {
+    return <div>There was an error</div>;
+  }
+  return <ProductDetailView product={data} />;
+};
 
 export default ProductDetail;
